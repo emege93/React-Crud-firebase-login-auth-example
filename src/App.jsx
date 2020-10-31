a8804b06a646877906a5012cb6bca7f5991cfc78
emege93 <emege93@gmail.com>
2020-10-31 11:20:28 +0100

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="container">
-        navbar
+        <Navbar/>
         <Switch>
           <Route path="/login">
             login
