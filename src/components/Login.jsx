f2a9c0c57fbfccb729019c1b928df4155b1ce2fc
emege93 <emege93@gmail.com>
2020-10-31 11:57:44 +0100

    validation and switch login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,55 +2,85 @@ import React, {useState} from 'react'
 
 const Login = () => {
 
-const [email, setEmail] = useState('')
-const [pass, setPass] = useState('')
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState(null)
+    const [esRegistro, setEsRegistro] = useState(true)
 
-const procesarDatos = e => {
-    e.preventDefault()
-    if(!email.trim()){
-        console.log('Ingrese Email');
-    }
-    if(!pass.trim()){
-        console.log('Ingrese Contraseña');
-    }
-    if(pass.length < 6){
-        console.log('Contraseña mayor a 6 caracteres');
-    }
-    console.log('Todo OK');
-}
 
-    return (
-        <div className="mt-5">
-            <h3 className="text-center">Acceso o Registros de usuarios</h3>
-            <hr/>
-            <div className="row justify-content-center">
-                <div className="col-12 col-sm-8 col-md-6-col-xl-4">
-                    <form onSubmit={procesarDatos}>
-                        <input
-                            type="email"
-                            className="form-control mb-2"
-                            placeholder="Ingrese su email"
-                            onChange={e => setEmail(e.target.value)}
-                            value={email}
-                        />
-                        <input
-                            type="password"
-                            className="form-control mb-2"
-                            placeholder="Ingrese su contraseña"
-                            onChange={e => setPass(e.target.value)}
-                            value={pass}
-                        />
-                        <button className="btn btn-dark btn-lg btn-block">
-                            Registrar
-                        </button>
-                        <button className="btn btn-info btn-sm btn-block">
-                            ¿Ya tienen cuenta?
-                        </button>
-                    </form>
+    const procesarDatos = e => {
+        e.preventDefault()
+        if(!email.trim()){
+            //console.log('Ingrese Email');
+            setError('Ingrese Email')
+            return
+        }
+        if(!pass.trim()){
+            //console.log('Ingrese Contraseña');
+            setError('Ingrese Contraseña')
+            return
+        }
+        if(pass.length < 6){
+            //console.log('Contraseña de 6 caracteres o mas');
+            setError('Contraseña de 6 caracteres o mas')
+            return
+        }
+
+        setError(null)
+        console.log('Todo OK');
+    }
+
+        return (
+            <div className="mt-5">
+                <h3 className="text-center">
+                    {
+                        esRegistro ? 'Registro de usuario' : 'Login de acceso'
+                    }
+                </h3>
+                <hr/>
+                <div className="row justify-content-center">
+                    <div className="col-12 col-sm-8 col-md-6-col-xl-4">
+                        <form onSubmit={procesarDatos}>
+                            {
+                                error && (
+                                    <div className="alert alert-danger">
+                                        {error}
+                                    </div>
+                                )
+                            }
+                            <input
+                                type="email"
+                                className="form-control mb-2"
+                                placeholder="Ingrese su email"
+                                onChange={e => setEmail(e.target.value)}
+                                value={email}
+                            />
+                            <input
+                                type="password"
+                                className="form-control mb-2"
+                                placeholder="Ingrese su contraseña"
+                                onChange={e => setPass(e.target.value)}
+                                value={pass}
+                            />
+                            <button className="btn btn-dark btn-lg btn-block" type="submit">
+                                {
+                                    esRegistro ? 'Registrarse' : 'Acceder'
+                                }
+                            </button>
+                            <button
+                                className="btn btn-info btn-sm btn-block"
+                                onClick={() => setEsRegistro(!esRegistro)}
+                                type="button"
+                            >
+                                {
+                                    esRegistro ? '¿Ya estas registrado?' : '¿No tienes cuenta?'
+                                }
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
 
 export default Login
